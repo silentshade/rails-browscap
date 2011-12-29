ea10a9eecfdc299c0aa80f98821feb85b3b17d95
silentshade <lifeemulation@gmail.com>
2011-12-29 14:05:05 +0400

    Boolean types fixes

--- lib/browscap_compat.rb
+++ lib/browscap_compat.rb
@@ -51,10 +51,10 @@ class Browscap
         browser.crawler               = properties['Crawler'].downcase == 'true'
         browser.css_version           = properties['CssVersion'].to_i
         # We can get a nil here, so just rely on CssVersion
-        browser.supports_css          = properties['CssVersion'].to_i > 0 ? 'true' : 'false'
+        browser.supports_css          = properties['CssVersion'].to_i > 0 ? true : false
         browser.aol_version           = properties['aolVersion'].to_i
         # Same here but rely on aolVersion
-        browser.aol                   = properties['aolVersion'].to_i > 0 ? 'true' : 'false'
+        browser.aol                   = properties['aolVersion'].to_i > 0 ? true : false
 
         @@user_agent_properties[section] = browser
 
